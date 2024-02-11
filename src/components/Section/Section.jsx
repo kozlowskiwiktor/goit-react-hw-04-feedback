920d8a0a0a91169547363e860b0b63d26e931e4b
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-11 10:16:46 +0100

    changed component Section
    changed Section from class component to function
    added propTypes

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import propTypes from 'react';
 
-export class Section extends Component {
-  render() {
-    const { title, children } = this.props;
-    return (
-      <div>
-        <h2>{title}</h2>
-        {children}
-      </div>
-    );
-  }
+function Section({ title, children }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
 }
+
+Section.propTypes = {
+  title: propTypes.string.isRequired,
+  children: propTypes.element,
+};
+
+export default Section;
